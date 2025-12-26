70d8068d184a8b75decc60f46cc8fcd5b76de29f
Matthew Russo <matthew@matthewclayrusso.com>
2025-12-25 19:03:00 -0800

    2025 : day 3, part 2

--- 2025/day_3/part2.c
+++ 2025/day_3/part2.c
@@ -15,7 +15,38 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // TODO: solution
+    uint64_t maximum_joltage = 0;
+
+    char buffer[4096];
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
+        size_t len = strlen(buffer);
+        if (buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
+            len -= 1;
+        }
+
+        char joltage_str[13] = {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '\0'};
+        for (int b_idx = 0; b_idx < len; b_idx++) {
+            uint8_t b_num = buffer[b_idx] - '0';
+            bool assigned = false;
+            for (int j_idx = 0; j_idx < 12; j_idx++) {
+                uint8_t j_num = joltage_str[j_idx] - '0';
+                if (assigned) {
+                    joltage_str[j_idx] = '0';
+                    continue;
+                }
+                if (b_num > j_num && len - b_idx >= 12 - j_idx) {
+                    joltage_str[j_idx] = buffer[b_idx];
+                    assigned = true;
+                }
+            }
+        }
+        uint64_t joltage = atoll(joltage_str);
+        maximum_joltage += joltage;
+        printf("Line: %s, JoltageStr: %s, Joltage: %llu, Maximum Joltage: %llu\n", buffer, joltage_str, joltage, maximum_joltage);
+    }
+
+    printf("Maximum Joltage: %llu\n", maximum_joltage);
 
     if (fclose(filePointer) == EOF) {
         perror("Error closing file");
@@ -26,3 +57,4 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+
