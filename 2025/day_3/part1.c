da84108cac0590e213856730195e8c9d6aa58c31
Matthew Russo <matthew@matthewclayrusso.com>
2025-12-25 18:19:48 -0800

    2025 : day 3, part 1

--- 2025/day_3/part1.c
+++ 2025/day_3/part1.c
@@ -15,7 +15,32 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // TODO: solution
+    uint64_t maximum_joltage = 0;
+
+    char buffer[4096];
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) {
+        size_t len = strlen(buffer);
+        if (buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
+            len -= 1;
+        }
+        uint8_t first_digit = 0;
+        uint8_t second_digit = 0;
+        for (int i = 0; i < len; i++) {
+            uint8_t num = buffer[i] - '0';
+            if (i != len - 1 && num > first_digit) {
+                first_digit = num;
+                second_digit = 0;
+            } else if (num > second_digit) {
+                second_digit = num;
+            }
+        }
+        uint8_t joltage = first_digit * 10 + second_digit;
+        maximum_joltage += joltage;
+        printf("Line: %s, Joltage: %d, Maximum Joltage: %llu\n", buffer, joltage, maximum_joltage);
+    }
+
+    printf("Maximum Joltage: %llu\n", maximum_joltage);
 
     if (fclose(filePointer) == EOF) {
         perror("Error closing file");
